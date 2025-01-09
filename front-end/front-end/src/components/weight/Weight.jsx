f872ff26dc6b6138a0ec32a28db397af13452025
jeries123 <jeriest59@gmail.com>
2025-01-09 10:49:30 +0200

    front-end

--- front-end/front-end/src/components/weight/Weight.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Weight() {
-  return (
-    <div>Weight</div>
-  )
-}
--- front-end/front-end/src/pages/DiseasePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function DiseasePage() {
-  return (
-    <div>DiseasePage</div>
-  )
-}
