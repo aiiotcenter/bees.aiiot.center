5c57ee7147f6ca85801433c7758ac02086e86486
Haseebcodejourney <hamzaneuraltec@gmail.com>
2025-02-11 11:25:33 +0200

    update

--- front-end/src/pages/BeePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import BeesTypes from '../components/Bees/BeesTypes'
-
-export default function BeePage({theme}) {
-  return (
-    <BeesTypes/>
-  )
-}
--- front-end/src/pages/WeightPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function WeightPage() {
-  return (
-    <div>WeightPage</div>
-  )
-}
