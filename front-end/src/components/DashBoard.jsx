2b579f6c5450dc92cb25f7dd5bc76cc92f2c36bb
Haseebcodejourney <hamzaneuraltec@gmail.com>
2025-02-14 15:25:30 +0200

    update

--- front-end/src/components/DashBoard.jsx
+++ front-end/src/components/DashBoard.jsx
@@ -26,7 +26,7 @@ export default function DashBoard() {
                     </CardsWrapper>
                 </Container>
             </PageWrapper>
-            <Notifications/>
+            {/* <Notifications/> */}
         </>
     )
 }
---
+++ front-end/src/components/Header/SideBar.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { FaTachometerAlt, FaHeart, FaEnvelope, FaStar, FaFileInvoice, FaMoneyCheck, FaFileAlt, FaCog, FaBriefcase, FaProjectDiagram, FaUser, FaSignOutAlt } from "react-icons/fa";
+
+// Styled Components
+const SidebarContainer = styled.div`
+  width: 100%;
+  max-width: 170px;
+  background-color: #fff;
+  padding: 15px;
+  border-radius: 10px;
+  position: fixed;
+  top: 70px; /* Positioned below the header */
+  transform: translate(-50%, ${({ isOpen }) => (isOpen ? "0" : "-10px")}) scale(${({ isOpen }) => (isOpen ? "1" : "0.95")});
+  opacity: ${({ isOpen }) => (isOpen ? "1" : "0")};
+  transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+  box-shadow: ${({ isOpen }) => (isOpen ? "0 5px 20px rgba(0,0,0,0.2)" : "none")};
+  z-index: 1000;
+  visibility: ${({ isOpen }) => (isOpen ? "visible" : "hidden")};
+  right: -100px;
+`;
+
+const MenuSection = styled.div`
+  margin-bottom: 20px;
+`;
+
+const MenuItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  font-size: 14px;
+  cursor: pointer;
+  color: #333;
+  border-radius: 5px;
+  transition: background 0.2s ease-in-out;
+
+  &:hover {
+    background: #f4f4f4;
+  }
+
+  &.active {
+    background: #000;
+    color: #fff;
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+const SideBar = ({ isOpen }) => {
+  return (
+    <SidebarContainer isOpen={isOpen}>
+
+
+
+
+      <MenuSection>
+        <MenuItem as={NavLink} to="/profile" className="active">
+          <FaUser/> My Profile
+        </MenuItem>
+        <MenuItem>
+          <FaSignOutAlt as={NavLink} to="/login"/> Logout
+        </MenuItem>
+      </MenuSection>
+    </SidebarContainer>
+  );
+};
+
+export default SideBar;
