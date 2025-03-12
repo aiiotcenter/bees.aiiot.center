238b6370219d0b0f5c1a5991cb25c2a2647f7152
Haseebcodejourney <haseebcodejourney@gmail.com>
2025-03-12 10:46:43 +0200

    update

--- front-end/src/components/Table.jsx
+++ front-end/src/components/Table.jsx
@@ -1,48 +1,106 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { PageWrapper, Table, Container } from '../Style/GlobalStyle';
+import { PageWrapper, Container } from '../Style/GlobalStyle';
 import { HeadingWrapper } from '../../src/Style/Dashboard/Style';
 import Typography from '../Style/Typography';
 import { MainWrapper } from '../Style/Table/Style';
+import styled from 'styled-components';
+
+// Styled Components
+const StyledTableWrapper = styled.div`
+  max-height: 500px;
+  overflow: hidden; /* Hide scroll */
+  border: 1px solid #ddd;
+`;
+
+// Table styles
+const StyledTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  table-layout: fixed; /* Keeps columns uniform */
+`;
+
+// Table Head - Fixed at top
+const TableHead = styled.thead`
+  background: #f4f4f4;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
+
+// Table Body - Scrollable but hides scrollbar
+const TableBody = styled.tbody`
+  display: block;
+  max-height: 500px;
+  overflow-y: scroll;
+  scrollbar-width: none; /* Hide scrollbar (Firefox) */
+  -ms-overflow-style: none; /* Hide scrollbar (IE/Edge) */
+
+  &::-webkit-scrollbar {
+    display: none; /* Hide scrollbar (Chrome/Safari) */
+  }
+`;
+
+// Table Row - Maintain structure
+const TableRow = styled.tr`
+  display: table;
+  width: 100%;
+  table-layout: fixed; /* Uniform column sizing */
+`;
+
+// Header & Cell Styling - Centered and same size
+const TableHeaderCell = styled.th`
+  padding: 12px;
+  border: 1px solid #ddd;
+  text-align: center;
+  width: 50%;
+`;
+
+const TableCell = styled.td`
+  padding: 12px;
+  border: 1px solid #ddd;
+  text-align: center;
+  width: 50%;
+`;
 
 export default function TableComponent() {
   const location = useLocation();
   const { selectedData } = location.state || { selectedData: [] };
 
   return (
-<>
-<PageWrapper>
+    <PageWrapper>
       <Container>
         <HeadingWrapper>
           <Typography variant="h1">Details</Typography>
           <Typography variant="p">Below is the data related to the selected card.</Typography>
         </HeadingWrapper>
         <MainWrapper>
-          <Table>
-            <thead>
-              <tr>
-                <th style={{ width: '25%' }}>Index</th>
-                <th style={{ width: '75%' }}>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {selectedData.length > 0 ? (
-                selectedData.map((item, index) => (
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{Object.values(item)[0]}</td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td colSpan="100%" style={{ textAlign: 'center', display:'table-cell' }}>No data available</td>
-                </tr>
-              )}
-            </tbody>
-          </Table>
+          <StyledTableWrapper>
+            <StyledTable>
+              <TableHead>
+                <TableRow>
+                  <TableHeaderCell>Index</TableHeaderCell>
+                  <TableHeaderCell>Value</TableHeaderCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {selectedData.length > 0 ? (
+                  selectedData.map((item, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{index + 1}</TableCell>
+                      <TableCell>{Object.values(item)[0]}</TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan="2">No data available</TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </StyledTable>
+          </StyledTableWrapper>
         </MainWrapper>
       </Container>
     </PageWrapper>
-</>
   );
 }
